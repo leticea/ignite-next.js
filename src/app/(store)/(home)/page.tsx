48d2ee5c06bb7853217471c92c4de02387077fb1
leticea <nascimento.leticeah@gmail.com>
2023-11-19 15:11:24 -0300

    feat(home): creating home page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,5 +1,47 @@
-export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+import Image from "next/image";
+import Link from "next/link";
 
-  return <h1>Hello World</h1>;
+export default function Home() {
+  return (
+    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
+      <Link
+        href="/"
+        className="col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          alt=""
+          src="/moletom-never-stop-learning.png"
+          width={920}
+          height={920}
+          quality={100}
+        />
+      </Link>
+
+      <Link
+        href="/"
+        className="col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          alt=""
+          src="/moletom-java.png"
+          width={920}
+          height={920}
+          quality={100}
+        />
+      </Link>
+
+      <Link
+        href="/"
+        className="col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          alt=""
+          src="/camiseta-dowhile-2022.png"
+          width={920}
+          height={920}
+          quality={100}
+        />
+      </Link>
+    </div>
+  );
 }
