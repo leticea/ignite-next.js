60d551588f86934490894bcc0be340d82d6776b4
leticea <nascimento.leticeah@gmail.com>
2023-11-22 20:13:37 -0300

    feat(slug/route.ts): creating product route

---
+++ src/app/api/products/[slug]/route.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+import data from "../data.json";
+
+export async function GET(
+  _: Request,
+  { params }: { params: { slug: string } }
+) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const slug = z.string().parse(params.slug);
+
+  const product = data.products.find((product) => product.slug === slug);
+
+  return Response.json(product);
+}
