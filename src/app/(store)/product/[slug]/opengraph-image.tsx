d8927cb70649a9d27233d7612ef15ca260f72142
leticea <nascimento.leticeah@gmail.com>
2023-11-28 20:36:35 -0300

    feat(opengraph-image.tsx): generate open graph image

--- src/app/(store)/product/[slug]/opengraph-image.tsx
+++ src/app/(store)/product/[slug]/opengraph-image.tsx
@@ -1,6 +1,8 @@
 import { api } from "@/data/api";
 import { Product } from "@/data/types/product";
+import { env } from "@/env";
 import { ImageResponse } from "next/og";
+import { zinc } from "tailwindcss/colors";
 
 export const runtime = "edge";
 
@@ -22,23 +24,27 @@ async function getProduct(slug: string): Promise<Product> {
   return products;
 }
 
-export default async function Image({ params }: { params: { slug: string } }) {
+export default async function OgImage({
+  params,
+}: {
+  params: { slug: string };
+}) {
   const product = await getProduct(params.slug);
+
+  const productImageURL = new URL(product.image, env.APP_URL).toString();
 
   return new ImageResponse(
     (
       <div
         style={{
-          fontSize: 128,
-          background: "white",
+          background: zinc[950],
           width: "100%",
           height: "100%",
           display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
+          flexDirection: "column",
         }}
       >
-        About Acme
+        <img src={productImageURL} alt="" style={{ width: "100%" }} />
       </div>
     ),
     {
