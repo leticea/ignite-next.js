7732d7db177e22dc768425f0b2b7ec6c090eb2cf
leticea <nascimento.leticeah@gmail.com>
2023-12-02 16:28:35 -0300

    feat(search/route.ts): search route

--- src/app/api/products/search/route.ts
+++ src/app/api/products/search/route.ts
@@ -1,20 +1,21 @@
 import { z } from "zod";
 import data from "../data.json";
+import type { NextRequest } from "next/server";
 
-export async function GET(
-  request: Request,
-) {
+export async function GET(request: NextRequest) {
   await new Promise((resolve) => setTimeout(resolve, 1000));
 
-  
+  const { searchParams } = request.nextUrl;
 
-  const slug = z.string().parse(params.slug);
+  const query = z.string().parse(searchParams.get("q"));
 
   const product = data.products.find((product) => product.slug === slug);
 
-  if (!product) {
-    return Response.json({ message: "Product not found." }, { status: 400 });
-  }
+  const products = data.products.filter((product) => {
+    return product.title
+      .toLocaleLowerCase()
+      .includes(query.toLocaleLowerCase());
+  });
 
-  return Response.json(product);
+  return Response.json(products);
 }
