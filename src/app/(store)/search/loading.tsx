ce44885a708662a22af66b7c5b03b1a24a16bd98
leticea <nascimento.leticeah@gmail.com>
2023-12-02 18:11:34 -0300

    feat(search/loading.tsx): search loading

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -6,7 +6,7 @@ import { useSearchParams } from "next/navigation";
 export default function SearchLoading() {
   const searchParams = useSearchParams();
 
-  const { q: query } = searchParams;
+  const query = searchParams.get("q");
 
   return (
     <div className="flex flex-col gap-4">
