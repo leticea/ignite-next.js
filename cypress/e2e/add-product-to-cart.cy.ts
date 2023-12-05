30d8784d73ac94278fc587709f99215dfa8750f5
leticea <nascimento.leticeah@gmail.com>
2023-12-05 19:37:27 -0300

    feat(search.cy.ts): inserting uncaught exception to prevent a failure

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -7,8 +7,8 @@ describe("add product to cart", () => {
     cy.get('a[href^="/product"]').first().click();
 
     cy.location("pathname").should("include", "/product");
-    cy.contains("Adicionar ao carrinho").click();
 
+    cy.contains("Adicionar ao carrinho").click();
     cy.contains("Cart (1)").should("exist");
   });
 
@@ -19,19 +19,16 @@ describe("add product to cart", () => {
 
     cy.contains("Adicionar ao carrinho").click();
     cy.contains("Adicionar ao carrinho").click();
-
     cy.contains("Cart (1)").should("exist");
   });
 
   it("should be able to search for a product and add it to the cart", () => {
     cy.get("input[name=q]").type("moletom").parent("form").submit();
-
     cy.get('a[href^="/product"]').first().click();
 
     cy.location("pathname").should("include", "/product");
 
     cy.contains("Adicionar ao carrinho").click();
-
     cy.contains("Cart (1)").should("exist");
   });
 });
