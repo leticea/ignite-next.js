0e211a46bfccf0df5c37c4d47b009527ede429a0
leticea <nascimento.leticeah@gmail.com>
2023-12-05 19:23:19 -0300

    feat(search.cy.ts): inserting uncaught exception to prevent a failure

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -1,9 +1,7 @@
 describe("search products", () => {
-  beforeEach(() => {
-    cy.visit("/");
-  });
-
   it("should be able to search for products", () => {
+    cy.visit("/");
+
     cy.get("input[name=q]").type("moletom").parent("form").submit();
 
     cy.location("pathname").should("include", "/search");
@@ -13,11 +11,12 @@ describe("search products", () => {
   });
 
   it("should not be able to visit search page without a search query", () => {
-    cy.get("input[name=q]").type("moletom").parent("form").submit();
+    cy.on("uncaught:exception", () => {
+      return false;
+    });
 
-    cy.location("pathname").should("include", "/search");
-    cy.location("search").should("include", "q=moletom");
+    cy.visit("/search");
 
-    cy.get('a[href^="/product"]').should("exist");
+    cy.location("pathname").should("equal", "/");
   });
 });
